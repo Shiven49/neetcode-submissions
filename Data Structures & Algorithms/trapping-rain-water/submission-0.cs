a436557b28fcf244aa17e5b35ec60ec258649bce
Shiven49 <114616039+Shiven49@users.noreply.github.com>
2026-07-22 17:02:28 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int Trap(int[] height) {
+        int l=0;
+        int r=height.Length-1;
+        int lm=0;
+        int rm=0;
+        int water=0;
+        while(l<r){
+            if(height[l]<height[r]){
+                if(height[l]> lm){
+                    lm=height[l];
+                }
+                else{
+                    water+=lm-height[l];
+                }
+                l++;
+            }
+            else{
+                if(height[r]>=rm){
+                    rm=height[r];
+                }
+                else{
+                    water+=rm-height[r];
+                }
+                r--;
+            }
+        }
+        return water;
+    }
+}
