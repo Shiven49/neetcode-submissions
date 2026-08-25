d75f6f0ce7441fe930ae1712a503db6c1ba086e6
Shiven49 <114616039+Shiven49@users.noreply.github.com>
2026-08-26 01:22:43 +0530

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.cs
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+public class Solution {    
+    public ListNode MergeKLists(ListNode[] lists) {
+        if (lists.Length == 0)
+
+            return null;
+
+        int interval = 1;
+
+        while (interval < lists.Length) {
+
+            for (int i = 0; i + interval < lists.Length; i += interval * 2) {
+
+                ListNode list1 = lists[i];
+
+                ListNode list2 = lists[i + interval];
+
+                ListNode dummy = new ListNode(0);
+
+                ListNode curr = dummy;
+
+                while (list1 != null && list2 != null) {
+
+                    if (list1.val <= list2.val) {
+
+                        curr.next = list1;
+
+                        list1 = list1.next;
+
+                    }
+
+                    else {
+
+                        curr.next = list2;
+
+                        list2 = list2.next;
+
+                    }
+
+                    curr = curr.next;
+
+                }
+
+                if (list1 != null)
+
+                    curr.next = list1;
+
+                else
+
+                    curr.next = list2;
+
+                lists[i] = dummy.next;
+
+            }
+
+            interval *= 2;
+
+        }
+
+        return lists[0];
+    }
+}
